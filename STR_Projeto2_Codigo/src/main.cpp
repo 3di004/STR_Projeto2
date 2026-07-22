b4241bfc63807e2511eff08cb650bd761156ee2a
Edileudo Filho <134215014+3di004@users.noreply.github.com>
2026-07-22 15:27:45 -0300

    EDI - 22/07/26 - C3
    Terceiro commit inicial do projeto.
    - Adicionadas linhas para teste posterior do FreeRTOS com a lógica de funcionamento do projeto 1.

--- STR_Projeto2_Codigo/src/main.cpp
+++ STR_Projeto2_Codigo/src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
 
+#define botao1 4
+#define botao2 16
+#define botao3 17
+#define led1 2
+#define led2 5
+#define led3 18
+#define led4 19
+
 //Configurações do sistema
 int capacidade_patio = 2;
 int total_no_patio = 0;
@@ -10,12 +18,23 @@ void trem_produtor(int);
 
 void setup() {
 	Serial.begin(9600); //Iniciando comunicação serial para monitoramento do sistema.
-	pinMode(3, OUTPUT); //Saída digital:
-	pinMode(4, OUTPUT); //Saída digital:
+	pinMode(botao1, INPUT_PULLUP); //Entrada digital:
+	pinMode(botao2, INPUT_PULLUP); //Entrada digital:
+	pinMode(botao3, INPUT_PULLUP); //Entrada digital:
+	pinMode(led1, OUTPUT); //Saída digital:
+	pinMode(led2, OUTPUT); //Saída digital:
+	pinMode(led3, OUTPUT); //Saída digital:
+	pinMode(led4, OUTPUT); //Saída digital:
 }
 
 void loop() {
-	
+	if (!botao1){
+		trem_produtor(1);
+	} else if (!botao2){
+		trem_produtor(2);
+	} else if (!botao3){
+		trem_produtor(3);
+	}
 }
 
 void trem_produtor(int linha) {
@@ -23,17 +42,21 @@ void trem_produtor(int linha) {
 
 	if (xSemaphoreTake(trilho_compartilhado, portMAX_DELAY) == pdTRUE){ //O if só é executado quando o trilho estiver vago.
 		Serial.print("[Linha "); Serial.print(linha); Serial.println("] Trem entrou no trilho compartilhado.");
+
 		vTaskDelay(pdMS_TO_TICKS(2500)); //Tempo de travessia do trilho.
+
 		Serial.print("[Linha "); Serial.print(linha); Serial.println("] Trem chegou ao fim do trilho. Aguardando pela descarga no pátio.");
 		
 		if (xSemaphoreTake(vagas_patio, portMAX_DELAY) == pdTRUE){ //No final do trilho compatilhado, o trem espera a entrada no pátio. Durante esse tempo, outro trem não pode entrar no trilho compartilhado.
 			xSemaphoreGive(trilho_compartilhado); //O trem entra no pátio e o trilho compatilhado torna-se disponível
 			total_no_patio++;
-			Serial.print("[Linha "); Serial.print(linha); Serial.println("] Trem descarregando...");
+			Serial.print("[Linha "); Serial.print(linha); Serial.println("] Trem entrou no pátio. Descarregando...");
+
 			vTaskDelay(pdMS_TO_TICKS(3000)); //Tempo que o trem passa descarregando.
+
 			xSemaphoreGive(vagas_patio);
-			Serial.print("[Linha "); Serial.print(linha); Serial.println("] Trem descarregou! Saindo do pátio e voltando à mina.");
 			total_no_patio--;
+			Serial.print("[Linha "); Serial.print(linha); Serial.println("] Trem descarregou! Saindo do pátio e voltando à mina.");
 		}
 	}
 }
